877828a6ec5e1cd7d2d56e7541d8a8ffbdcc33f3
hugo <hugo.juvin0202@gmail.com>
2025-05-13 11:45:58 +0200

    knn_v3

--- prediction_methods/knn.py
+++ prediction_methods/knn.py
@@ -4,19 +4,12 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-def tune_knn_hyperparameters(data: pd.DataFrame):
-    data = format_dataset(data,[0,-24,-48],1)
-    y = data['alerte']
-    X = data[['timestamp_h0', 'Valeur_h0', 'timestamp_h-24', 'Valeur_h-24', 'timestamp_h-48', 'Valeur_h-48']].copy()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=random_state)
-
-    y_train = y_train.replace(0, -1)
-    y_test = y_test.replace(0, -1)
+def tune_knn_hyperparameters(x, y):
 
     # Define a broader range of parameters to test
     param_grid = {
-        'n_neighbors': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+        'n_neighbors': [3, 5, 7, 9, 11, 13, 15],
         'weights': ['uniform', 'distance'],
         'metric': ['euclidean', 'manhattan', 'chebyshev', 'minkowski'], # Common metrics
         'p': [1, 2, 3] # Relevant for minkowski metric
@@ -28,32 +21,28 @@ def tune_knn_hyperparameters(data: pd.DataFrame):
     # cv=5 means 5-fold cross-validation
     # scoring='accuracy' is the metric used to evaluate models
     grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='accuracy', n_jobs=-1) # n_jobs=-1 uses all available cores
-    grid_search.fit(X_train, y_train)
+    grid_search.fit(x, y)
 
     return grid_search.best_params_
 
+def train_model_knn(data: pd.DataFrame) -> KNeighborsClassifier:
+    data = format_dataset(data,[0,-24,-48])
+    y = data['alerte_d+1']
+    x = data[['timestamp_h0', 'Valeur_h0', 'timestamp_h-24', 'Valeur_h-24', 'timestamp_h-48', 'Valeur_h-48']].copy()
 
-def train_model_knn(data: pd.DataFrame):
-    best_params = tune_knn_hyperparameters(data)
-
-    data = format_dataset(data,[0,-24,-48],1)
-    y = data['alerte']
-    X = data[['timestamp_h0', 'Valeur_h0', 'timestamp_h-24', 'Valeur_h-24', 'timestamp_h-48', 'Valeur_h-48']].copy()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=random_state)
-
-    y_train = y_train.replace(0, -1)
-    y_test = y_test.replace(0, -1)
+    best_params = tune_knn_hyperparameters(x, y)
 
     # Train the model using the best parameters found
     knn = KNeighborsClassifier(**best_params)
-    knn.fit(X_train, y_train)
+    knn.fit(x, y)
 
-    return knn, X_test, y_test
+    return knn
+
+def get_model_knn_error(model: KNeighborsClassifier, data: pd.DataFrame) -> float:
+    x = format_dataset(data,[0,-24,-48])[['timestamp_h0', 'Valeur_h0', 'timestamp_h-24', 'Valeur_h-24', 'timestamp_h-48', 'Valeur_h-48']].copy()
 
 
-def get_model_knn_error(model: KNeighborsClassifier, X_test: pd.DataFrame, y_test: pd.Series) -> float:
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+    y_pred = model.predict(x)
+    accuracy = accuracy_score(y, y_pred)
     error_rate = 1 - accuracy
     return error_rate
