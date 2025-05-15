349e0c1a376b21635b69abfd2ca742e987b54f4b
P-Joss-P <jossgamer684@gmail.com>
2025-05-15 09:19:43 +0200

    Fix cross_validation_arbre.py

--- prediction_methods/cross_validation_arbre.py
+++ prediction_methods/cross_validation_arbre.py
@@ -2,6 +2,7 @@ from pandas import DataFrame
 
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
 
@@ -12,8 +13,8 @@ import numpy as np
 
 def cross_validation_arbre_classification(x, y) -> int:
     param_grid = {
-        "depth_array" : np.arange(1,20)
-        "metric" : ['gini']
+        "depth_array" : np.arange(1, 20)
+        "metric" : ['gini', 'entropy']
     }
     My_tree_class = DecisionTreeClassifier()
     grid_search = GridSearchCV(My_tree_class, cv=5, scoring='accuracy', n_jobs=-1)
@@ -24,14 +25,14 @@ def cross_validation_arbre_classification(x, y) -> int:
 
 
 
-def cross_validation_arbre_regression(data: DataFrame) -> int:
+def cross_validation_arbre_regression(x, y) -> int:
 
     param_grid = {
-        "depth_array" : np.arange(1,20)
-        "metric" : ['squared_error']
+        "depth_array" : np.arange(1, 20)
+        "metric" : ['squared_error', 'friedman_mse']
     }
     My_tree_class = DecisionTreeRegressor()
-    grid_search = GridSearchCV(My_tree_class, cv=5, scoring='accuracy', n_jobs=-1)
+    grid_search = GridSearchCV(My_tree_class, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
 
     grid_search.fit(x, y)
 
