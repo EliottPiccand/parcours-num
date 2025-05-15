c75277edf3bea7356aa0889da1543c42e530581f
hugo <hugo.juvin0202@gmail.com>
2025-05-15 10:05:29 +0200

    knn_classification

--- prediction_methods/__init__.py
+++ prediction_methods/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 # from .model_arbre import train_model_arbre_classification, get_model_error_arbre
 # from .model_arbre import train_model_arbre_regression
-from .knn import train_model_knn, get_model_knn_error
+from .knn_classification import train_model_knn, get_model_knn_error
 
 if TYPE_CHECKING:
     from typing import Any
